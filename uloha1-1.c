8f68eeba03eb3b783224d9f04cb7a78d517748ed
Matej Hrnčiar <matej.hrnciar2@gmail.com>
2022-02-22 12:57:12 +0100

    Add exercises from 1st class

---
+++ uloha1-1.c
@@ -0,0 +1,71 @@
+/*
+ * Humanitárny konvoj sa presúva na Ukrajinu do hlavného mesta Kyjev, aby pomohol tunajším ľuďom. Ako
+ * jeden z organizátorov si sa dozvedel, že konvoj má prechádzať cez most s nosnosťou N. Vieš, že na
+ * most sa nezmestí viac ako K vozidiel súčasne. Zároveň poznáš hmotnosť každého vozidla. (Naopak
+ * nevieš vopred povedať akým spôsobom budú vozidlá cez most prechádzať). Tvojou úlohou je zistiť,
+ * či je prejazd cez most pre konvoj bezpečný, alebo nie.
+ *
+ * Na vstupe je niekoľko riadkov, každý obsahuje čísla N, K, M a M čísel h[i], ktoré určujú hmotnosť
+ * vozidiel konvoja. Pre každý riadok na vstupe vypíšte Ano ak konvoj môže bezpečne prejsť cez most
+ * bez ohľadu nato v akom poradí budú vozidlá cez most prechádzať, inak vypíšte Nie.
+ *
+ * Obmedzenia:
+ * 1 <= N <= 10000,
+ * 1 <= K <= M,
+ * 1 <= M <= 9,
+ * 1 <= h[i] <= 1000
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG 0
+
+int cmpfunc(const void *a, const void *b) {
+    return (*(int *) b - *(int *)a);
+}
+
+int main() {
+    int capacity, max_vehicles, n_cars;
+
+    while (scanf("%d", &capacity) > 0) {
+        scanf("%d %d", &max_vehicles, &n_cars);
+
+        int *cars = (int *) malloc(sizeof(int) * n_cars);
+
+        for (int i = 0; i < n_cars; i++) {
+            scanf("%d", &cars[i]);
+        }
+
+        if (DEBUG) {
+            printf("Original: \n");
+            for (int i = 0; i < n_cars; i++) {
+                printf("%d ", cars[i]);
+            }
+            printf("\n");
+        }
+
+        qsort(cars, n_cars, sizeof(int), cmpfunc);
+
+        if (DEBUG) {
+            printf("Sorted: \n");
+            for (int i = 0; i < n_cars; i++) {
+                printf("%d ", cars[i]);
+            }
+            printf("\n");
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < max_vehicles; i++) {
+            sum += cars[i];
+        }
+
+        if (sum <= capacity)
+            printf("Ano\n");
+        else
+            printf("Nie\n");
+
+        free(cars);
+    }
+}
---
+++ uloha1-2.c
@@ -0,0 +1,91 @@
+/*
+ * Skupina Iron Maiden po koncerte na Topfeste sa vracia naspäť domov do Londýna. Teraz sa potrebujú
+ * zbaliť. Majú veľa vecí - kostýmy, aparatúru, gitary, bicie, pareničky a korbáčiky,... K dispozícii
+ * majú ľubovoľný počet kamiónov, no keďže zaplatenie každého kamiónu niečo stojí, chcú minimalizovať
+ * celkové náklady. Každý kamión má nosnosť 300 krabíc. Keďže chcú mať vo veciach poriadok, balia ich
+ * spolu do väčších balíkov, každý pojme 101 až 300 krabíc. Tvojou úlohou je zistiť koľko najmenej
+ * kamiónov potrebujú na úspešné prepravenie vecí (zabalených vo väčších balíkoch) domov.
+ *
+ * Každý riadok obsahuje celé číslo N predstavujúce počet väčších balíkov do ktorých sú zabalené veci,
+ * a N čísel x[i], kde x[i] je počet krabíc zabalených v i-tom balíku. Pre každý riadok vypíšte jedno
+ * číslo - minimálny počet kamiónov, ktoré musíme zaplatiť na prevezenie všetkých krabíc (ktoré sú
+ * balené vo väčších balíkoch).
+ *
+ * Obmedzenia:
+ * 1 <= N <= 50,
+ * 101 <= x[i] <= 300
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG 0
+
+int main() {
+    int n_boxes;
+
+    while (scanf("%d", &n_boxes) > 0) {
+        int *boxes = (int *) malloc(sizeof(int) * n_boxes);
+
+        for (int i = 0; i < n_boxes; i++) {
+            scanf("%d", &boxes[i]);
+        }
+
+        if (DEBUG) {
+            printf("Loaded: \n");
+            for (int i = 0; i < n_boxes; i++) {
+                printf("%d ", boxes[i]);
+            }
+            printf("\n");
+        }
+
+        int trucks = 0;
+        int *buckets = (int *) calloc(200, sizeof(int));
+
+        for (int i = 0; i < n_boxes; i++) {
+            if (boxes[i] >= 200)
+                trucks++;
+            else
+                buckets[boxes[i]]++;
+        }
+
+        if (DEBUG) {
+            printf("Trucks: %d\n", trucks);
+            printf("Buckets: \n");
+            for (int i = 101; i < 200; i++) {
+                printf("%d: %d\n", i, buckets[i]);
+            }
+            printf("\n");
+        }
+
+        int front = 101, back = 199;
+        while (front <= back) {
+            if (buckets[front] > 0 && buckets[back] > 0) {
+                if (front + back <= 300) {
+                    buckets[front]--;
+                    buckets[back]--;
+                    trucks++;
+                }
+                else {
+                    buckets[back]--;
+                    trucks++;
+                }
+            }
+            else if (buckets[front] > 0 && buckets[back] == 0) {
+                back--;
+            }
+            else if (buckets[front] == 0 && buckets[back] > 0) {
+                front++;
+            }
+            else {
+                front++;
+                back--;
+            }
+        }
+
+        printf("%d\n", trucks);
+
+        free(boxes);
+        free(buckets);
+    }
+}
