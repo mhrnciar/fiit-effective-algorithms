066db5cedf80605ea31827939c540c46e3ca669b
Matej Hrnčiar <matej.hrnciar2@gmail.com>
2022-04-08 11:43:52 +0200

    Finish 5-1, update 5-2

--- uloha5-1.c
+++ uloha5-1.c
@@ -30,17 +30,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define DEBUG 1
+#define DEBUG 0
+
+// Compare first pos-2 numbers in array with the currently selected number and add 1 to position,
+// if the difference is bigger than the limit
+int *check_pos(int *arr, int pos, int limit, int *count) {
+    int *out = (int *) malloc(pos * sizeof(int));
+
+    for (int i = 0; i < pos - 1; i++) {
+        if (abs(arr[i] - arr[pos - 1]) >= limit) {
+            out[ (*count)++ ] = i + 1;
+        }
+    }
+
+    if (DEBUG) {
+        printf("pos: %d, ", pos);
+        for (int i = 0; i < pos; i++) {
+            printf("%d ", out[i]);
+        }
+        printf("\n");
+    }
+
+    return out;
+}
 
 int main() {
     int n, limit;
 
     while (scanf("%d %d", &n, &limit) > 0) {
+        int min = 1000, max = 0, found = 0;
         int *arr = (int *) malloc(n * sizeof(int));
 
+        // Read n numbers from input while saving max and min
         for (int i = 0; i < n; i++) {
             scanf("%d", &arr[i]);
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
         }
 
         if (DEBUG) {
@@ -50,6 +81,43 @@ int main() {
             }
             printf("\n");
         }
+
+        // If the difference between max and min is lower than limit,
+        // or there are only 2 exercises, print n (must solve all exercises)
+        if (max - min < limit || n <= 2) {
+            printf("%d\n", n);
+            continue;
+        }
+
+        // First exercise always has to be solved, so start from the second number, but since we can
+        // omit one exercise, if we solved the previous one, we will compare the i+1-th position.
+        // List of positions where the difference is higher than limit is returned along with the count
+        for (int i = 1; i < n; i++) {
+            int count = 0, best = 999;
+            int *res = check_pos(arr, i + 1, limit, &count);
+
+            // If count is not 0 (some solution has been found), calculate the number of exercises between
+            // min and max + 1, if the result is smaller than current best, update the best
+            if (count != 0) {
+                for (int j = 0; j < count; j++) {
+                    int steps = floor((double) res[j] / 2);
+                    steps += ceil((double) ((i + 1) - res[j]) / 2) + 1;
+                    best = steps < best ? steps : best;
+                }
+
+                // Set flag to found and print the best solution
+                printf("%d\n", best);
+                found = 1;
+                break;
+            }
+        }
+
+        // If best hasn't been found, print n
+        if (!found) {
+            printf("%d\n", n);
+        }
+
+        free(arr);
     }
 
     return 0;
--- uloha5-2.c
+++ uloha5-2.c
@@ -25,10 +25,115 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define DEBUG 1
 
+int **arr;
+
+void evaluate(const char string[50], unsigned long len) {
+    for (int j = 1; j < len; j += 2) {
+        int k = j;
+        for (int i = 0; k < len; i++, k++) {
+            if (k == 1 && string[k] == '<') {
+                arr[i][k] = 1;
+            }
+            else if (k == len-1 && string[k-1] == '>') {
+                arr[i][k] = 1;
+            }
+            else if (k != 1 && k != len-1 && (string[k-1] != '<' || string[k] !='>')) {
+                arr[i][k] = 1;
+            }
+            else if (j != 1) {
+                for (int row = i+2, col = i+1; row < k && col < k; row += 2, col += 2) {
+                    if (arr[i][col] == 1 && arr[col][k] == 1) {
+                        arr[i][k] = 1;
+                        break;
+                    }
+                }
+                if (arr[i][k] != 1 && (string[i] != '<' || string[k+1] !='>')) {
+                    arr[i][k] = 1;
+                }
+                else {
+                    arr[i][k] = 0;
+                }
+            }
+            else {
+                arr[i][k] = 0;
+            }
+        }
+    }
+}
+
 int main() {
+    char string[50], result[50];
+
+    for (int i = 0; i < 50; i++) {
+        result[i] = '.';
+    }
+
+    while(scanf("%s", string) > 0) {
+        char *num = string;
+        unsigned long len = strlen(num);
+
+        arr = (int **) calloc(len, sizeof(int *));
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int *) calloc(len, sizeof(int));
+            for (int j = 0; j < len; j++) {
+                arr[i][j] = -1;
+            }
+        }
+
+        if (DEBUG) {
+            for (int i = 0; i < len; i++) {
+                printf("%c ", string[i]);
+            }
+            printf("\n");
+        }
+
+        evaluate(string, len);
+
+        if (DEBUG) {
+            for (int i = 0; i < len; i++) {
+                for (int j = 0; j < len; j++) {
+                    printf("%2d ", arr[i][j]);
+                }
+                printf("\n");
+            }
+        }
+
+        for (int j = 1; j < len; j += 2) {
+            int k = j;
+            for (int i = 0; k < len; i++, k++) {
+                if (arr[i][k] == 0) {
+                    if (k == 1) {
+                        result[0] = 'o';
+                    }
+                    else if (j != 1) {
+                        result[k+j-1] = 'o';
+                    }
+                    else {
+                        result[k] = 'o';
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            printf("%c", result[i]);
+        }
+        printf("\n");
+
+        for (int i = 0; i < 50; i++) {
+            string[i] = '\0';
+            result[i] = '.';
+        }
+
+        for (int i = 0; i < len; i++) {
+            free(arr[i]);
+        }
+        free(arr);
+    }
 
     return 0;
 }
